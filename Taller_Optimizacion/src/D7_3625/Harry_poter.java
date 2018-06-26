4c5c15eff6c1493c63d217cddc72c45782c5c4c7
Ana Victoria Cavazos Argot <anavca@outlook.com>
2018-06-26 18:01:58 -0500

    -

---
+++ Taller_Optimizacion/src/D7_3625/Harry_poter.java
@@ -0,0 +1,30 @@
+package D7_3625;
+
+import java.util.Scanner;
+
+public class Harry_poter {
+
+	public static void main(String[] args) {
+		// 3625 - Harry Potter and the Stones
+		Scanner sc = new Scanner(System.in);
+		int T=sc.nextInt();
+		String respuesta="";
+		
+		for(int i=0;i<T;i++)
+		{
+			double n =sc.nextInt();
+			System.out.println();
+			System.out.println(Math.log(n));
+			if(Math.log(n)>1) {
+				respuesta+="\nRon wins";
+			}
+			else
+			{
+				respuesta+="\nHarry wins";
+			}
+		}
+		System.out.println(respuesta);
+		
+	}
+
+}
