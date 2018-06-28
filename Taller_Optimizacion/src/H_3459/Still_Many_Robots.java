1170a9db818364d3e56d55a985c2295e25727a5f
Ana Victoria Cavazos Argot <anavca@outlook.com>
2018-06-28 00:55:17 -0500

    -

---
+++ Taller_Optimizacion/src/H_3459/Still_Many_Robots.java
@@ -0,0 +1,32 @@
+package H_3459;
+
+import java.util.Scanner;
+
+public class Still_Many_Robots {
+
+	static long P[] = new long[21];
+	static long Q[] = new long[21];
+	
+	public static void pre_calculo() {
+		P[1]=1;
+		Q[1]=0;
+		for(int i=2;i<21;i++) {
+			P[i]=(i*P[i-1])+1;
+			Q[i]=Q[i-1]+(i*(i*i));
+			System.out.println(i + "| P:"+P[i]+ "| Q:"+Q[i]);
+		}
+	}
+	
+	public static void main(String[] args) {
+		// 3459 - Still Many Robots
+		Scanner sc = new Scanner(System.in);
+		pre_calculo();
+		int T = sc.nextInt();
+		for(int i=0;i<T;i++) {
+			int N = sc.nextInt();
+			System.out.println(P[N] +" "+Q[N]);
+		}
+	}
+
+}
+ 
